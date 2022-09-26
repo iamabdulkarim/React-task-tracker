8ae6a39961dd47eed25a53ab6067b8ab39db473c
iamabdulkarim <abdulkarim34.fk@gmail.com>
2022-09-26 13:10:15 +0600

    Compoonent props with default props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import  Header  from "./components/Header";
 function App() {
+  
   return (
     <div className="App">
-      <h1>Hello form react</h1>
+      <Header title='hello karim'/>
     </div>
+    
   );
 }
 
